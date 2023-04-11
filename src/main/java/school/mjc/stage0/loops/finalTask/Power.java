4691c310cc457dbd837238d3d437b41083d8609a
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-11 22:29:18 +0300

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        
+        int i;
+        int b = 1;
+        for (i=1; i <= power; i++){
+            b= b * numberToPrint;
+            
+        }
+        System.out.println(b);
+            
 
     }
 
